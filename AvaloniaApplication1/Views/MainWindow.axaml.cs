b62f13b88b7a817c3e527110fbd709976eeb1c92
hate deadynasty <sherstobitov.11@bk.ru>
2025-05-16 03:05:05 +0500

    ХЕХЕЕЕ

--- AvaloniaApplication1/Views/MainWindow.axaml.cs
+++ AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml.Templates;
 using AvaloniaApplication1.ViewModels;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using System;
 using Yandex.Music.Api;
 using Yandex.Music.Client;
@@ -15,7 +17,11 @@ namespace AvaloniaApplication1.Views
             InitializeComponent();
             DataContext = new MainWindowViewModel();
 
-            
+            var box = MessageBoxManager
+.GetMessageBoxStandard("Caption", "Are you sure you would like to delete appender_replace_page_1?",
+    ButtonEnum.YesNo);
+            var result = box.ShowAsync();
         }
+
     }
 }
